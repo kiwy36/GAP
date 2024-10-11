3dacf90924da14611745bab1c150180a10917f2f
kiwy36 <kevinwittner92@gmail.com>
2024-10-10 23:29:17 -0300

    comentarios nuevos

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,37 +1,46 @@
-// src/components/Login.js
 import { useState } from 'react';
 import { auth, signInWithEmailAndPassword } from '../../services/firebase.js';
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2'; // Librería para mostrar alertas interactivas
 import './Login.css'
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  // Estados para almacenar el email, la contraseña y posibles errores
+  const [email, setEmail] = useState(''); // Estado para almacenar el email del usuario
+  const [password, setPassword] = useState(''); // Estado para almacenar la contraseña del usuario
+  const [error, setError] = useState(''); // Estado para almacenar errores de inicio de sesión
 
+  // Función que maneja el proceso de inicio de sesión
   const handleLogin = async () => {
+    // Validación: si el email o la contraseña están vacíos, muestra un error
     if (!email || !password) {
       setError('Por favor, complete todos los campos');
       return;
     }
 
-    setError('');
+    setError(''); // Limpia el mensaje de error previo
+
     try {
+      // Llamada a Firebase para autenticar al usuario con email y contraseña
       await signInWithEmailAndPassword(auth, email, password);
+      
+      // Muestra una alerta de éxito si la autenticación es correcta
       Swal.fire({
         title: "¡Bienvenido!",
         text: "Usuario autenticado exitosamente",
         icon: "success"
       });
     } catch (error) {
+      // Si ocurre un error, se captura y se muestra un mensaje de error
       console.error('Error en el inicio de sesión:', error);
       setError('Error en el inicio de sesión: ' + error.message);
     }
   };
 
+  // Renderiza la interfaz de usuario para el formulario de inicio de sesión
   return (
     <div className="login-container">
       <h2>Iniciar Sesión</h2>
+      {/* Campo de entrada para el email */}
       <input 
         type="email" 
         placeholder="Email" 
@@ -39,6 +48,7 @@ const Login = () => {
         onChange={(e) => setEmail(e.target.value)} 
         className="input-field"
       />
+      {/* Campo de entrada para la contraseña */}
       <input 
         type="password" 
         placeholder="Contraseña" 
@@ -46,7 +56,9 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)} 
         className="input-field"
       />
+      {/* Muestra un mensaje de error si existe */}
       {error && <p className="error-message">{error}</p>}
+      {/* Botón para iniciar sesión */}
       <button onClick={handleLogin} className="login-button">Iniciar Sesión</button>
     </div>
   );
