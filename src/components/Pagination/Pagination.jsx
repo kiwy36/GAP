4f524f8ed7ed4888227efff5ecacc53a70f37d57
kiwy36 <kevinwittner92@gmail.com>
2024-10-18 18:45:56 -0300

    avancePagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,30 +1,49 @@
 import PropTypes from 'prop-types';
 import './Pagination.css'; // Estilos específicos para la paginación
 
-const Pagination = ({ totalProducts, pageSize, currentPage, onPageChange }) => {
-    const totalPages = Math.ceil(totalProducts / pageSize);
-
-    if (totalPages <= 1) return null;
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const getPaginationGroup = () => {
+        const groupSize = 5; // Número mínimo de páginas visibles
+        let start = Math.floor((currentPage - 1) / groupSize) * groupSize + 1;
+        let end = Math.min(start + groupSize - 1, totalPages);
+        return Array.from({ length: end - start + 1 }, (_, idx) => start + idx);
+    };
 
     return (
         <div className="pagination">
-            {Array.from({ length: totalPages }, (_, i) => (
+            {/* Botón de navegación hacia atrás */}
+            <button
+                onClick={() => onPageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                &lt;
+            </button>
+
+            {/* Renderiza los números de las páginas */}
+            {getPaginationGroup().map(page => (
                 <button
-                    className={`pagination-button ${currentPage === i + 1 ? 'active' : ''}`}
-                    key={i}
-                    onClick={() => onPageChange(i + 1)}
+                    key={page}
+                    onClick={() => onPageChange(page)}
+                    className={page === currentPage ? 'active' : ''}
                 >
-                    {i + 1}
+                    {page}
                 </button>
             ))}
+
+            {/* Botón de navegación hacia adelante */}
+            <button
+                onClick={() => onPageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+            >
+                &gt;
+            </button>
         </div>
     );
 };
 
 Pagination.propTypes = {
-    totalProducts: PropTypes.number.isRequired,
-    pageSize: PropTypes.number.isRequired,
     currentPage: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired,
     onPageChange: PropTypes.func.isRequired,
 };
 
